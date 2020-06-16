c1679988c0b4696f13d7f6b37d901b1115f4332b
Wxin96 <18706306187@163.com>
2020-06-16 21:35:53 +0800

    项目创建

---
+++ src/test/java/com/apollo/flashsale/FlashSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apollo.flashsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashSaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
