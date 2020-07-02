336ce3deb70159f0e3539926c6cb9eba7f5c7b67
Wxin96 <18706306187@163.com>
2020-07-02 11:01:24 +0800

    war包方式

---
+++ src/main/java/com/apollo/flashsale/ServletInitializer.java
@@ -0,0 +1,11 @@
+package com.apollo.flashsale;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(FlashSaleApplication.class);
+    }
+}
